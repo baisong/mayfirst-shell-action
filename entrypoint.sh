99d9b19ad18daeba3007fd1f0d6306f6f77445bc
Oren Robinson <get.oren@gmail.com>
2024-01-04 21:01:47 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,15 +8,17 @@ echo "time=$time" >> $GITHUB_OUTPUT
 ! which jq >/dev/null && printf "Please install jq.\n" && exit 1
 
 url=https://members.mayfirst.org/cp/api.php
-password_file="$1"
+deploy_comment="$1"
 user="$2"
 password="$3"
-echo "url $url"
-echo `expr substr ${url} 0 5`
-echo "user $2"
-echo `expr substr ${2} 0 5`
-echo "password $3"
-echo `expr substr ${3} 0 5`
+echo "comment $deploy_comment"
+echo "${deploy_comment}" | cut -c1-5
+echo "url: $url"
+echo "${url}" | cut -c1-5
+echo "user: ${user}"
+echo "${user}" | cut -c1-5
+echo "password: ${password}"
+echo "${password}" | cut -c1-5
 
 if [ -z "$user" -o -z "$password" ]; then
   printf "Failed to locate user and password in password file.\n"
