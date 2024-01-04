935298f6135626f0f97462bd858f50fdeaf793b3
Oren Robinson <get.oren@gmail.com>
2024-01-04 21:30:07 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
 
-echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 
@@ -12,18 +11,6 @@ deploy_comment="$1"
 user="$2"
 password="$3"
 echo "comment $deploy_comment"
-echo "${deploy_comment}" | cut -c1-5
-echo "url: $url"
-echo "${url}" | cut -c1-5
-echo "user: ${user}"
-echo "${user}" | cut -c1-5
-echo "password: ${password}"
-echo "${password}" | cut -c1-5
-
-if [ -z "$user" -o -z "$password" ]; then
-  printf "Failed to locate user and password in password file.\n"
-  exit 1
-fi
 
 out=$(curl --silent "${url}" -X POST -d "user_name=$user" -d "user_pass=$password" -d "action=grant")
 exit=$(echo "$out" | jq .is_error)
